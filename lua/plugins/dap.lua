29db95e520ef2e5cb9769ca5d8703797ce9b259a
yoptohlejepeta <peta.kotlan@seznam.cz>
2025-06-22 23:10:26 +0200

    default settings

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -21,7 +21,7 @@ return {
       local dap = require("dap")
       local dapui = require("dapui")
 
-      vim.fn.sign_define('DapBreakpoint', { text = '❌', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapBreakpoint', { text = '⏭ ', texthl = '', linehl = '', numhl = '' })
 
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
@@ -35,58 +35,9 @@ return {
         dapui.close()
       end
 
-      dapui.setup({
-        icons = { expanded = "▾", collapsed = "▸", current_frame = "→" },
-        mappings = {
-          expand = { "<CR>", "<2-LeftMouse>" },
-          open = "o",
-          remove = "d",
-          edit = "e",
-          repl = "r",
-          toggle = "t",
-        },
-        layouts = {
-          {
-            elements = {
-              { id = "scopes", size = 0.25 },
-              "breakpoints",
-              "stacks",
-              "watches",
-            },
-            size = 40,
-            position = "left",
-          },
-          {
-            elements = {
-              "repl",
-              "console",
-            },
-            size = 0.25,
-            position = "bottom",
-          },
-        },
-        floating = {
-          max_height = nil,
-          max_width = nil,
-          border = "rounded",
-          mappings = {
-            close = { "q", "<Esc>" },
-          },
-        },
-      })
+      dapui.setup()
 
-      require("nvim-dap-virtual-text").setup({
-        enabled = true,
-        enabled_commands = true,
-        highlight_changed_variables = true,
-        highlight_new_as_changed = false,
-        show_stop_reason = true,
-        commented = false,
-        virt_text_pos = 'eol',
-        all_frames = false,
-        virt_lines = false,
-        virt_text_win_col = nil
-      })
+      require("nvim-dap-virtual-text").setup()
 
       vim.keymap.set('n', '<F5>', function() dap.continue() end, { desc = 'Debug: Continue' })
       vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end, { desc = 'Debug: Toggle Breakpoint' })
