cc6caebcf6b44e0342b2a5f99fbf60489937c61b
yoptohlejepeta <peta.kotlan@seznam.cz>
2025-07-13 18:58:27 +0200

    autopair better than mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,5 +1,16 @@
 return {
-  { 'echasnovski/mini.move',     version = false, config = function() require('mini.move').setup() end },
-  { 'echasnovski/mini.surround', version = false, config = function() require('mini.surround').setup() end },
-  { 'echasnovski/mini.pairs',    version = false, config = function() require("mini.pairs").setup() end },
+	{
+		"echasnovski/mini.move",
+		version = false,
+		config = function()
+			require("mini.move").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
 }
